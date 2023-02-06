daf412accdce7facf8620e70832d55f4c18d14fe
richiethie <richiethie@gmail.com>
2023-02-05 21:12:37 -0600

    finished projects page view

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -20,22 +20,61 @@ const ProjectInfo = () => {
                     <h4 className="projectPageTitle">Featured Projects</h4>
                 </div>
                 <div className="projectList">
-                    <div className="we-do projectCard">
-                        <h6 className='projectCardTitle'>WE DO</h6>
-                        <img src={weDoImage} className="projectImages"></img>
+                    <div className="flip-card projectCard">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={weDoImage} className="projectImages"></img>
+                            </div>
+                            <div className="flip-card-back">
+                                <a href="https://github.com/richiethie" target="_blank" className="projectLink" >
+                                    <h6 className='projectCardTitle'>WE DO</h6>
+                                </a>
+                                
+                            </div>
+                        </div> 
                     </div>
-                    <div className="mood-for-food projectCard">
-                        <h6 className='projectCardTitle'>MOOD FOR FOOD</h6>
-                        <img src={moodForFoodImage} className="projectImages"></img>
+                    <div className="flip-card projectCard">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={moodForFoodImage} className="projectImages"></img>
+                            </div>
+                            <div className="flip-card-back">
+                                <a href="https://github.com/richiethie" target="_blank" className="projectLink">
+                                    <h6 className='projectCardTitle2'>MOOD FOR FOOD</h6>
+                                </a>
+                                
+                            </div>
+                        </div> 
                     </div>
-                    <div className="weather-dashboard projectCard">
-                        <h6 className='projectCardTitle'>WEATHER DASHBOARD</h6>
-                        <img src={weatherDashboardImage} className="projectImages"></img>
+                    <div className="flip-card projectCard">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={weatherDashboardImage} className="projectImages"></img>
+                            </div>
+                            <div className="flip-card-back">
+                                <a href="https://github.com/richiethie" target="_blank" className="projectLink">
+                                    <h6 className='projectCardTitle3'>WEATHER DASHBOARD</h6>
+
+                                </a>
+                                
+                            </div>
+                        </div> 
                     </div>
-                    <div className="work-day-scheduler projectCard">
-                        <h6 className='projectCardTitle'>WORK DAY SCHEDULER</h6>
-                        <img src={workDaySchedulerImage} className="projectImages"></img>
+                    <div className="flip-card projectCard">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={workDaySchedulerImage} className="projectImages"></img>
+                            </div>
+                            <div className="flip-card-back">
+                                <a href="https://github.com/richiethie" target="_blank" className="projectLink">
+                                    <h6 className='projectCardTitle4'>WORK DAY SCHEDULER</h6>
+
+                                </a>
+                                
+                            </div>
+                        </div> 
                     </div>
+                    
                 </div>
             </div>
         </div>
